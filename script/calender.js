301e1b47a8c3c4d7aeea1c40bd7187af79b8a836
elham rajaee <elham.rajairad@gmail.com>
2022-05-25 18:23:02 +0430

    add calender to project

--- script/calender.js
+++ script/calender.js
@@ -593,3 +593,29 @@
   //attach to global window object
   global.dycalendar = dycalendar;
 })(typeof window !== "undefined" ? window : this);
+
+dycalendar.draw({
+  target: "#calendar",
+  type: "month",
+  dayformat: "full",
+  monthformat: "full",
+  highlighttargetdate: true,
+  prevnextbutton: "show",
+});
+
+(function (i, s, o, g, r, a, m) {
+  i["GoogleAnalyticsObject"] = r;
+  (i[r] =
+    i[r] ||
+    function () {
+      (i[r].q = i[r].q || []).push(arguments);
+    }),
+    (i[r].l = 1 * new Date());
+  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m);
+})(window, document, "script", "//www.google-analytics.com/analytics.js", "ga");
+
+ga("create", "UA-46156385-1", "cssscript.com");
+ga("send", "pageview");
